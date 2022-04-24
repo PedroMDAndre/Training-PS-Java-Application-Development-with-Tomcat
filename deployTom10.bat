441d861fe274e73e6003f88e8fc1febab18c3cb8
PedroMDAndre <pedro.mda@gmail.com>
2022-04-24 01:06:26 +0100

    update deploy to Tomcat 10 script

--- deployTom10.bat
+++ deployTom10.bat
@@ -1,5 +1,14 @@
 @echo off
+SET PROJECT_PATH=D:\training\Java Web apps\target
+SET TOMCAT_PATH=C:\Java\Tomcat\tomcat-10.0.20
+
+echo 0: Package project
 call mvn package
-echo 1: Copy/deploy war file
-xcopy /y "D:\training\Java Web apps\target\my_servlet.war" "C:\Java\Tomcat\tomcat-10.0.20\webapps\"
+
+echo 1: Copy/deploy war files
+xcopy /y "%PROJECT_PATH%\my_servlet.war" "%TOMCAT_PATH%\webapps\"
+xcopy /y "%PROJECT_PATH%\my_jsp.war" "%TOMCAT_PATH%\webapps\"
+xcopy /y "%PROJECT_PATH%\my_jstl.war" "%TOMCAT_PATH%\webapps\"
+xcopy /y "%PROJECT_PATH%\my_filters.war" "%TOMCAT_PATH%\webapps\"
+xcopy /y "%PROJECT_PATH%\my_listener.war" "%TOMCAT_PATH%\webapps\"
 
